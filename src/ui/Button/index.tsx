7531b56164b561fbd61075f6e7fa0b47c6f4318c
adriany1997 <yasnoadriandatos@gmail.com>
2023-11-26 13:36:19 -0500

    Change version

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -21,14 +21,15 @@ const Button = ({
 }: OverridableFoxyButton) => {
   const ref = useRef<HTMLParagraphElement | null>(null);
 
-  const buttonClass = classNames(
+  args.className = classNames(
     gStyle[`${color}`],
     lStyle.element,
     lStyle[`${color}`],
     lStyle.container,
     lStyle[`rounded-${rounded}`],
     lStyle[rounded ? "rounded" : ""],
-    lStyle[`${variant}`]
+    lStyle[`${variant}`],
+    args.className
   );
 
   const contentClass = classNames(
@@ -39,14 +40,12 @@ const Button = ({
       variant === "outlined" ? `border-${border || 1}` : `border-${border}`
     ],
     lStyle[`${size}`],
-    transform,
-    args.className
+    transform
   );
 
-  delete args.className;
   return (
     <>
-      <button className={buttonClass} {...args} type={args.type || "button"}>
+      <button {...args} type={args.type || "button"}>
         <p ref={ref} className={contentClass}>
           {startIcon && <span>{startIcon}</span>}
           {children && <>{children}</>}
