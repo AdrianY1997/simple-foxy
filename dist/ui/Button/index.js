5f3357b82378303702a186c52b49de7c2d4bc991
adriany1997 <yasnoadriandatos@gmail.com>
2023-11-26 13:50:25 -0500

    build

--- dist/ui/Button/index.js
+++ dist/ui/Button/index.js
@@ -33,8 +33,8 @@ const global_module_css_1 = __importDefault(require("../global.module.css"));
 const button_module_css_1 = __importDefault(require("./button.module.css"));
 const Button = ({ size, variant = "text", color = "default", startIcon, endIcon, children, transform, rounded = "mn", border, ...args }) => {
     const ref = (0, react_1.useRef)(null);
-    args.className = (0, classnames_1.default)(global_module_css_1.default[`${color}`], button_module_css_1.default.element, button_module_css_1.default[`${color}`], button_module_css_1.default.container, button_module_css_1.default[`rounded-${rounded}`], button_module_css_1.default[rounded ? "rounded" : ""], button_module_css_1.default[`${variant}`], args.className);
-    const contentClass = (0, classnames_1.default)(button_module_css_1.default.content, button_module_css_1.default[`rounded-${rounded}`], button_module_css_1.default[rounded ? "rounded" : ""], button_module_css_1.default[variant === "outlined" ? `border-${border || 1}` : `border-${border}`], button_module_css_1.default[`${size}`], transform);
+    args.className = (0, classnames_1.default)(global_module_css_1.default[`${color}`], button_module_css_1.default.element, button_module_css_1.default[`${color}`], button_module_css_1.default.container, button_module_css_1.default[`rounded-${rounded}`], button_module_css_1.default[rounded ? "rounded" : ""], button_module_css_1.default[variant === "outlined" ? `border-${border || 1}` : `border-${border}`], button_module_css_1.default[`${variant}`], args.className);
+    const contentClass = (0, classnames_1.default)(button_module_css_1.default.content, button_module_css_1.default[`rounded-${rounded}`], button_module_css_1.default[rounded ? "rounded" : ""], button_module_css_1.default[`${size}`], transform);
     return (react_1.default.createElement(react_1.default.Fragment, null,
         react_1.default.createElement("button", { ...args, type: args.type || "button" },
             react_1.default.createElement("p", { ref: ref, className: contentClass },
